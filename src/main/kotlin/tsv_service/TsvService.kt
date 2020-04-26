10a3711ec19c4d8ad7b41413df3519ba4f639bb1
oks <churkinaksana@gmail.com>
2020-04-26 10:55:58 +0300

    get rs-id in fun findBy

--- src/main/kotlin/tsv_service/TsvService.kt
+++ src/main/kotlin/tsv_service/TsvService.kt
@@ -1,11 +1,14 @@
 package tsv_service
 
+
+//import io.ktor.http.HttpHeaders.Connection
+import io.ktor.http.HttpHeaders.Connection
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import tsv_model.Tsv
 import tsv_model.tsvTable
-import java.sql.Connection
+//import java.sql.Connection
 
 
 class TsvService {
@@ -17,10 +20,13 @@ class TsvService {
         mutation = row[tsvTable.mutation],
         rs = row[tsvTable.rs] )
 
+ /*
+ * findBy осуществляет поиск по базе 42.tsv
+ * возвращает  rs-идентификатор из базы, по соответствующим аргументам
+ * */
 
-
-    fun findBy(Contig: String, Left: Int,  Mutation: String, Right: Int)  =
-         transaction(transactionIsolation = 8 ,
+    fun findBy(Contig: String, Left: Int,  Mutation: String, Right: Int): String  =
+         transaction(transactionIsolation = 8,
              repetitionAttempts = 0) {
             tsvTable.select {
                 tsvTable.contig.eq(Contig) and
@@ -30,6 +36,7 @@ class TsvService {
            }
           .map { toTsv(it) }
                 .first()
+                    .rs
         }
 
     }
