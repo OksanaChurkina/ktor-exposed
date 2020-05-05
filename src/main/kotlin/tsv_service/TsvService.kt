bf591e51790211fdaf7f9dcb33db20436e4203ce
oks <churkinaksana@gmail.com>
2020-05-05 16:47:42 +0300

    javadoc

--- src/main/kotlin/tsv_service/TsvService.kt
+++ src/main/kotlin/tsv_service/TsvService.kt
@@ -1,13 +1,11 @@
 package tsv_service
 
-
-//import io.ktor.http.HttpHeaders.Connection
-import io.ktor.http.HttpHeaders.Connection
+import com.typesafe.config.ConfigException
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import tsv_model.Tsv
 import tsv_model.tsvTable
+
 //import java.sql.Connection
 
 
@@ -20,26 +18,38 @@ class TsvService {
         mutation = row[tsvTable.mutation],
         rs = row[tsvTable.rs] )
 
- /*
- * findBy осуществляет поиск по базе 42.tsv
- * возвращает  rs-идентификатор из базы, по соответствующим аргументам
- * */
+ /**
+ findBy осуществляет поиск по базе 42.tsv
+ возвращает  rs-идентификатор из базы, по соответствующим аргументам
+ rs-identifier - аннотация генетического варианта
+ contig - contig name
+ left - inclusive zero-based position of the mutation left boundary
+ right - exclusive zero-based position of the mutation right boundary
+ mutation - mutation nucleotide sequence within the boundaries
+ */
 
-    fun findBy(Contig: String, Left: Int,  Mutation: String, Right: Int): String  =
-         transaction(transactionIsolation = 8,
-             repetitionAttempts = 0) {
-            tsvTable.select {
-                tsvTable.contig.eq(Contig) and
-                        tsvTable.left.eq(Left) and
-                        tsvTable.right.eq(Right) and
-                        tsvTable.mutation.eq(Mutation)
-           }
-          .map { toTsv(it) }
-                .first()
-                    .rs
+
+
+    fun findBy(Contig: String, Left: Int,  Mutation: String, Right: Int): String  {
+
+        return transaction(transactionIsolation = 8,
+                    repetitionAttempts = 0) {
+                tsvTable.select {
+                    tsvTable.contig.eq(Contig) and
+                            tsvTable.left.eq(Left) and
+                            tsvTable.right.eq(Right) and
+                            tsvTable.mutation.eq(Mutation)
+                }
+                        .map { toTsv(it) }
+                        .first()
+                        .rs
+            }
+
+
         }
-
     }
+
+
 
 
 
